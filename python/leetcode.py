f4840026f26fd2c616253be3976290e3eab826ea
sethezgreen <sethezgreen@gmail.com>
2023-09-24 18:46:15 -0400

    first commit

---
+++ python/leetcode.py
@@ -0,0 +1,13 @@
+# 1. TwoSum
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        n = len(nums)
+        for i in range(n-1):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+        return []
+    
+result = Solution()
+print(result.twoSum([2,5,9,3,1], 14))
