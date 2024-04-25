1669e115dbdc5b8447caedf547ea15488902cbb6
Seth Green <sethezgreen@gmail.com>
2024-04-25 09:37:32 -0400

    started lonely integer

---
+++ hacker-rank/lonelyInteger/lonelyInteger.js
@@ -0,0 +1,8 @@
+// pseudo code
+    // recieve an array of integers, where each will occur twice besides one
+    // find and return the one integer that does not occur twice
+    // will need a way to keep track of how many occurrences there are of each element
+    // iterate through array once
+        // store the values already seen
+        // if value has been seen, ignore it
+        // value that has not been seen becomes the return
